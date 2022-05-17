02f0d46af1cdba485301807585ff54ca36caf624
lazymisha <misha_trunov@hotmail.com>
2022-05-17 16:24:59 +0300

    - added CI

--- src/routes/delete.ts
+++ src/routes/delete.ts
@@ -28,7 +28,7 @@ router.delete(
     await order.save();
 
     // publishing an event saying this was cancelled!
-    await new OrderCancelledPublisher(natsWrapper.client).publish({
+    new OrderCancelledPublisher(natsWrapper.client).publish({
         id: order.id,
         version: order.version,
         ticket: {
